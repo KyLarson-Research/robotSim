66f203a4b2c2b27f5b3096ed1af893a0d9710b68
k <Kylel@email.arizona.edu>
2023-11-06 14:42:55 -0700

    functional

--- main.c
+++ main.c
@@ -1,11 +1,34 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#include<math.h>
+#include<math.h> // do not forget to compile : gcc main.c -lm
 #include"robot.h"
 //#include"robot.c"
 #define PI 3.141592654
 int main(void){
+   //----------------Check to make sure that robot produces x_next 0.05 y_next 0.0 and theta_next is 0.01
+   /*double x =0;
+   double y =0;
+   double theta =0;
+   double v =0.5, w =0.1;
+   double x_next, y_next, theta_next;
+   printf("\n%lf %lf %lf\n", x, y, theta);
+   robot(0.1, x, y, theta, v, w, &x_next, &y_next, &theta_next);
+   printf("\n%lf %lf %lf\n", x_next, y_next, theta_next);*///end of check above -----------------------
    
+   double T, x, y, theta, v[50], w[50], x_next, y_next, theta_next;
+   int k=0;
+   scanf("%lf %lf %lf %lf", &T, &x, &y, &theta);
+   x_next = x;
+   y_next = y;
+   theta_next = theta;
+   for(k=1;scanf("%lf %lf", &v[k], &w[k]) == 2; k++){
+      robot(T, x_next, y_next, theta_next, v[k], w[k], &x_next, &y_next, &theta_next);
+      //if(k==5 || k==10){
+         printf("%lf: %.4lf, %.4lf, %.4lf\n", T*k, x_next, y_next, theta_next);
+      //}
+   }
+   robot(T, x_next, y_next, theta_next, v[k], w[k], &x_next, &y_next, &theta_next);
+   printf("%lf: %lf, %lf, %lf\n", T*k, x_next, y_next, theta_next);
    return 0;
 }
--- robot.c
+++ robot.c
@@ -2,7 +2,7 @@
 #include<math.h>//remember to compile with –lm
 
 double X_k_1(double x_k, double T, double theta_k, double v_k, double w_k){
-   if(w_k == 0){
+   if(fabs(w_k) < 0.000000001){
       return x_k + T*cos(theta_k)*v_k;
    }
    else{
@@ -11,16 +11,16 @@ double X_k_1(double x_k, double T, double theta_k, double v_k, double w_k){
 }
 
 double Y_k_1(double y_k, double T, double theta_k, double v_k, double w_k){
-   if(w_k == 0){
+   if(fabs(w_k) < 0.000000001){
       return y_k + T*sin(theta_k)*v_k;
    }
    else{
-      return y_k + (v_k/w_k)*(cos(theta_k + T*(w_k))-cos(theta_k));
+      return y_k - (v_k/w_k)*(cos(theta_k + T*(w_k))-cos(theta_k));
    }
 }
 
 double theta_k_1(double theta_k, double T, double v_k, double w_k){
-   if(w_k == 0){
+   if(fabs(w_k) < 0.000000001){
       return theta_k;
    }
    else{ 
